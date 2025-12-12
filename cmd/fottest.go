319779f8c7b94344bfa6716fd7fc64098d8e3a09
yungyu <yungyu16@qq.com>
2025-12-12 10:21:19 +0800

    feat

--- cmd/fottest.go
+++ cmd/fottest.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	ints := [3]int{1, 2, 3}
@@ -10,14 +12,32 @@ func main() {
 	fmt.Println("-------------")
 	i3 := []int{4, 5, 6}
 	for i, k := range i3 {
+		if i%2 == 0 {
+			continue
+		}
 		fmt.Println(i)
 		i3 = append(i3, k)
 	}
 	fmt.Println("-------------")
-	i2 := []int{1, 2, 3}
-	for i := 0; i < len(i2); i++ {
-		i2 = append(i2, i2[i])
-		fmt.Println("=")
+	switch 3 {
+	case 1:
+		fmt.Println("1")
+	case 2:
+		fmt.Println("2")
+	case 3:
+		fmt.Println("3")
+		if int(2+1) == 3 {
+			break
+		}
+		fmt.Println("333")
+	default:
+		fmt.Println("4")
 	}
-	fmt.Println("-------------")
+	if "aaa" > "abc" {
+		print("1111")
+	} else {
+		print("2222")
+	}
+	fmt.Println("end")
+	fmt.Println([5]int{1, 2})
 }
